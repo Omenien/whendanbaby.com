e558f7bb2417d456c9e1ca46627c6f9e76c2a726
Edward Jones <edwardrjones97@gmail.com>
2021-10-19 21:43:49 +0000

    Update fun facts

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ const FUN_FACTS = [
   'Thomas Edison created the first light bulb',
   'Bryan Salesky invented Argo AI',
   'John Smodic was born',
-  'Brad Krupa released his best selling novel "The Butter Battle Book" before receding in to irrelevancy'
+  'Brad Krupa released his best selling novel "The Butter Battle Book" before receding in to irrelevancy',
+  '"What\'s Love Got To Do With It?" by Tina Turner won Record of the Year at the Grammy\'s',
+  'this website was created',
+  'Tom Brady won his first Super Bowl',
 ]
 
 function App() {
@@ -15,7 +18,7 @@ function App() {
     <main>
       <h1>dan was last a baby on September 17th, 1984</h1>
       <p>
-        Fun Fact: 1984 was the year ${funFact}.
+        Fun Fact: 1984 was the year {funFact}.
       </p>
     </main>
   );
